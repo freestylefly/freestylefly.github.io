455758da816017fe783cfe64c28081a0646390b4
freestylefly <2689458656@qq.com>
2020-01-05 23:14:55 +0800

    我又更新了(〃'▽'〃)

---
+++ README.md
@@ -0,0 +1,7 @@
+# 苍何的个人博客
+
+### 觉得不错的可以点歌star，如果不想麻烦，源码在我的[blog仓库](https://github.com/freestylefly/blog)
+
+### 直接clone我的仓库，本地跑起来，具体配置可以参考我的[这篇文章]([https://freestylefly.github.io/2019/07/10/hexo%E5%9C%A8GitHub%E4%B8%8A%E6%90%AD%E5%BB%BA%E4%B8%AA%E4%BA%BA%E5%8D%9A%E5%AE%A2/](https://freestylefly.github.io/2019/07/10/hexo在GitHub上搭建个人博客/))，如果想给博客添加自定义欢迎语，可以参考[文章]([https://freestylefly.github.io/2019/07/14/hexo%E5%8D%9A%E5%AE%A2%E7%BB%93%E5%90%88%E7%99%BE%E5%BA%A6%E8%AF%AD%E9%9F%B3%E5%90%88%E6%88%90%E4%B8%BA%E4%BD%A0%E7%9A%84%E5%8D%9A%E5%AE%A2%E6%B7%BB%E5%8A%A0%E6%AC%A2%E8%BF%8E%E8%AF%AD%E9%9F%B3/#more](https://freestylefly.github.io/2019/07/14/hexo博客结合百度语音合成为你的博客添加欢迎语音/#more))
+
+抽空会将readme更新完善下。
---
+++ dist/music.js
@@ -0,0 +1,97 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+	{
+        name: '欢迎您',
+        artist: '光临',
+        url: 'https://tsn.baidu.com/text2audio?tex=周颖我爱你，你好呀，欢迎您光临我的博客，有问题您可以点击右下角的在线客服，我会第一时间联系您，您也可以在留言区留言或者文章底部留言&lan=zh&cuid=123456&ctp=1&tok=24.9e25f97efb85031ea49b8799dbb3cce5.2592000.1580828796.282335-16799902',
+        cover: 'http://img.ytmp3.cn/image/8.jpg',
+      },
+	{
+        name: '黄沙',
+        artist: '名决',
+        url: 'http://www.ytmp3.cn/down/72418.mp3',
+        cover: 'http://img.ytmp3.cn/image/8.jpg',
+      },
+	{
+        name: '这个年纪',
+        artist: '齐一',
+        url: 'http://www.ytmp3.cn/down/35879.mp3',
+        cover: 'http://img.ytmp3.cn/image/22.jpg',
+      },
+	  {
+        name: '当真',
+        artist: '蒋家驹[蒋蒋]&曲肖冰',
+        url: 'http://www.ytmp3.cn/down/51714.mp3',
+        cover: 'http://img.ytmp3.cn/image/80.jpg',
+      },
+	{
+        name: "平凡之路",
+        artist: '朴树',
+        url: 'http://www.ytmp3.cn/down/59211.mp3',
+        cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
+      },
+	  {
+        name: '这些民谣 - 一次听个够',
+        artist: '翁大涵',
+        url: 'http://www.ytmp3.cn/down/60222.mp3',
+        cover: 'http://p2.music.126.net/Wx5GNJEpay2JbfVUJc4Aew==/109951163094853876.jpg?param=130y130',
+      },
+      {
+        name: '你的酒馆对我打了烊',
+        artist: '陈雪凝',
+        url: 'http://www.ytmp3.cn/down/59770.mp3',
+        cover: 'http://p1.music.126.net/LiRR__0pJHSivqBHZzbMUw==/109951163816225567.jpg?param=130y130',
+      },
+	  //爱毛毛，（毛不易）
+	  {
+        name: '像我这样的人',
+        artist: '毛不易',
+        url: 'http://www.ytmp3.cn/down/50667.mp3',
+        cover: 'http://img.ytmp3.cn/image/71.jpg',
+      },
+	  {
+        name: '消愁',
+        artist: '毛不易',
+        url: 'http://www.ytmp3.cn/down/53813.mp3',
+        cover: 'http://img.ytmp3.cn/image/29.jpg',
+      },
+	  {
+        name: '牧马城市',
+        artist: '毛不易',
+        url: 'http://www.ytmp3.cn/down/73199.mp3',
+        cover: 'http://img.ytmp3.cn/image/96.jpg',
+      },
+	  
+	  
+	  //爱五月天，人生永远五月天
+	  {
+        name: '突然好想你',
+        artist: '五月天',
+        url: 'http://www.ytmp3.cn/down/31795.mp3',
+        cover: 'http://img.ytmp3.cn/image/53.jpg',
+      },
+	  {
+        name: '你不是真正的快乐',
+        artist: '五月天',
+        url: 'http://www.ytmp3.cn/down/52105.mp3',
+        cover: 'http://img.ytmp3.cn/image/62.jpg',
+      },
+	  
+	  //回不去的记忆  剑网三
+	  {
+        name: '眉间雪',
+        artist: '诺言jason',
+        url: 'http://www.ytmp3.cn/down/39112.mp3',
+        cover: 'http://img.ytmp3.cn/image/75.jpg',
+      },
+	  {
+        name: '业火苍云歌',
+        artist: '诺言jason',
+        url: 'http://www.ytmp3.cn/down/37022.mp3',
+        cover: 'http://img.ytmp3.cn/image/51.jpg',
+      }
+    ]
+});
